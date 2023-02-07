dfdddf0be0d629c75fa93c4e0888aa7abad135dd
Kofi K <kofivincent026@gmail.com>
2023-02-07 12:21:05 +0000

    new file:   operators.js 	modified:   variables.html

---
+++ operators.js
@@ -0,0 +1,18 @@
+var a = 10;
+var b = 20.60;
+var c = 198;
+
+var add = a + b + c
+var difference = a + b + c
+var div = a / c
+var mult = a * 1
+var remainder = c % a
+var increament = a++
+var decreament = d--
+
+// compoind assignments
+
+a += 30
+b -= 0.60
+c *= 2
+div /= 10
